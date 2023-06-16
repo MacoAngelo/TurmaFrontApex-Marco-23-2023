b2c33bd32cc86f3ef14eda4be639eaab8376151b
MacoAngelo <marco.angelo.blu@gmail.com>
2023-06-15 22:27:30 -0300

    ff

--- NodeJS/exemplo06/index.js
+++ NodeJS/exemplo06/index.js
@@ -4,6 +4,9 @@ const app = express(); // Executa o módulo do Express, subindo um servidor.
 const path = require('path')
 
 // Utilizar Bootstrap
+app.use(express.static('public'));
+
+// join -> Serve para ligar string. Pode substituir "pasta" + "subpasta" para path.Join(pasta, subpasta) -> Feito para diretórios
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
@@ -16,18 +19,39 @@ const bodyParser = require("body-parser");
 const Post = require("./Post");
 
 // BodyParse
-app.use(bodyParser.urlencoded({extended:false})) // Ditar que as requisições vão ter URL Encode
+app.use(bodyParser.urlencoded({ extended: false })) // Ditar que as requisições vão ter URL Encode
 app.use(bodyParser.json()) // Ditar que o envio de dados será em formato JSON
 
 // Rotas
-app.get("/cad", function(requisicao, resposta) {
-    resposta.render("formulario");
+app.get("/cad", function (requisicao, resposta) {
+    resposta.render("formulario",
+        {
+            creating: true,
+        });
 });
 
-app.get("/", function(requisicao, resposta) {
+app.get("/editRecord/:id", function (requisicao, resposta) {
+    Post.findOne(
+        {
+            where: {
+                id: requisicao.params.id,
+            }
+        }).then((post) => {
+            resposta.render("formulario",
+                {
+                    creating: false,
+                    id: post.id,
+                    titulo: post.titulo,
+                    conteudo: post.conteudo
+                }
+            );
+        });
+});
+
+app.get("/", function (requisicao, resposta) {
     Post.findAll().then(
         function (posts) {
-            resposta.render("home", {posts: posts})
+            resposta.render("home", { posts: posts })
         }
     );
 
@@ -36,7 +60,7 @@ app.get("/", function(requisicao, resposta) {
     // resposta.render("home", {posts: todasPostagens});
 });
 
-app.post("/add", function(requisicao, resposta) {
+app.post("/add", function (requisicao, resposta) {
     // Criar registro no banco
     Post.create(
         {
@@ -46,41 +70,52 @@ app.post("/add", function(requisicao, resposta) {
         }
     ).then(
         // Redireciona para a página inicial dps de criar o registro
-        function() {
+        function () {
             resposta.redirect("/");
         }
     ).catch(
         // Envia a mensagem de erro caso ocorra algum ao criar o registro
-        function(erro) {
+        function (erro) {
             resposta.send("Falha ao cadastrar nova postagem. Erro: " + erro)
         }
     );
+});
+
+app.post("/update/:id", function (requisicao, resposta) {
+    Post.update(
+        { titulo: requisicao.body.titulo, conteudo: requisicao.body.conteudo },
+        { where: { id: requisicao.params.id } }
+    ).then(function () {
+        resposta.redirect("/")
+    }).catch(function (erro) {
+        resposta.send("Falha ao alterar a postagem. Erro:  " + erro)
+    });
 });
 
 app.get("/deletar/:id", function (requisicao, resposta) {
     Post.destroy(
         {
             // Filtra a exclusão para somente registros que o id seja o passado.
-            where: { 
+            where: {
                 'id': requisicao.params.id,
             }
         }
     ).then(
-        function() {
+        function () {
             resposta.redirect("/")
         }
     ).catch(
-        function(erro) {
+        function (erro) {
             resposta.send("Falha ao excluir a postagem. Erro: " + erro);
         }
     );
 })
 
-// Template
-app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+// Template -> Necessário para utilizar HandleBars
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Finalizar configuração do servidor
-app.listen(8082, function() {
+app.listen(8082, function () {
     console.log("Servidor rodando!!");
 });
